f7450e42289189a6ff7127e0bb47145210d189fc
matheussvini <matheussvini@outlook.com>
2022-11-07 00:07:57 -0300

    Filters Tweets by username and requisition get /tweets with pagination

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
 
   if (!username || !avatar) {
-    return res.status(422).send("Todos os campos são obrigatórios");
-  }
-
-  if (arrUsers.find((u) => u.username === username)) {
-    return res.status(409).send("Usuário já cadastrado");
+    return res.status(400).send("Todos os campos são obrigatórios");
   }
 
   const user = {
@@ -32,7 +28,7 @@ app.post("/tweets", (req, res) => {
   const { username, tweet } = req.body;
 
   if (!username || !tweet) {
-    return res.status(422).send("Todos os campos são obrigatórios");
+    return res.status(409).send("Todos os campos são obrigatórios");
   }
 
   const newTweet = {
@@ -41,23 +37,49 @@ app.post("/tweets", (req, res) => {
   };
 
   arrTweets.unshift(newTweet);
-  console.log(arrTweets)
+  console.log(arrTweets);
   res.status(201).send("OK");
 });
 
 app.get("/tweets", (req, res) => {
+    let page = parseInt(req.query.page);
+    if(page < 1){
+        return res.status(400).send("Informe uma página válida!")
+    };
+    if((page -1) * 10 > arrTweets.length){
+        return res.status(400).send(`Só existem tweets até a página ${Math.trunc(arrTweets.length/10)+1}`)
+    }
+
   arrTweets.forEach(
     (e) => (e.avatar = arrUsers.find((u) => u.username === e.username).avatar)
   );
-  const page = [];
+  const tweetsWithAvatar = [];
 
-  for (let i = 0; i < 10 && i < arrTweets.length; i++) {
-    page.push(arrTweets[i]);
+    if(!page){
+        page = 1;
+    }
+
+  for (let i = (10 * page) - 9; i <= 10 * page && i < arrTweets.length; i++) {
+    tweetsWithAvatar.push(arrTweets[i-1]);
   }
-  res.send(page);
+  res.send(tweetsWithAvatar);
+});
+
+app.get("/tweets/:username", (req, res) => {
+  const user = req.params.username;
+  arrTweets.forEach(
+    (e) => (e.avatar = arrUsers.find((u) => u.username === e.username).avatar)
+  );
+
+  const userTweets = arrTweets.filter((e) => e.username === user);
+  const tweetsWithAvatar = [];
+
+  for (let i = 0; i < 10 && i < userTweets.length; i++) {
+    tweetsWithAvatar.push(userTweets[i]);
+  }
+
+  res.send(tweetsWithAvatar);
 });
 
 const port = 5000;
 app.listen(port, () => console.log(`App running in http://localhost:${port}`));
-
-
